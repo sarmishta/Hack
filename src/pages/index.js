4b53a3266b2d4fc906eb616c58d577a0a0f56c51
sarmishta <bsarmishta@gmail.com>
2018-09-07 16:09:52 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to my new Gatsby site.</p>
+    <p>Hello All !</p>
+    <p>I am Sarmishta from India, an incoming Fall 2018 international Grad Computer Science student at University of California, Davis.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
